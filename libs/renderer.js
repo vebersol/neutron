b098798287bf098bc598d0a72b153dffe6ae40d8
Vinicius Ebersol <vebersol@conrad-caine.com>
2016-05-21 16:32:33 -0300

    update renderer and add basic test patterns

--- libs/renderer.js
+++ libs/renderer.js
@@ -2,16 +2,22 @@ var fse = require('fs-extra');
 var path = require('path');
 var util = require('util');
 var handlebars = require('handlebars');
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
 
 var democritusRenderer = function (settings) {
-	var patterns = [];
-
+	var init = function () {
+		cleanPaths(function () {
+			getPatterns();
+		});
+	};
 	var getSettings = function (settings) {
 		var defaultSettings = {
 			patternsDir: './src/patterns',
 			allowedPatterns: ['atoms', 'molecules'],
 			fileExtension: '.handlebars',
-			encode: 'utf8'
+			encode: 'utf8',
+			publicPatternsPath: rootPath + '/public/patterns'
 		};
 		
 		return util._extend(defaultSettings, settings);
@@ -23,32 +29,49 @@ var democritusRenderer = function (settings) {
 	}
 
 	var getPatterns = function () {
-
 		fse.walk(settings.patternsDir)
 			.on('data', function(file) {
 				if (path.extname(file.path) === settings.fileExtension) {
 					try {
-						fse.readFile(file.path, settings.encode, function(err, source) {
-							if (source) {
-								var helperName = getHelperName(file);
-								var registerHelper = setHelper(helperName, source);
-								var template = handlebars.compile(source);
-								var data = {text: "lorem ipsum"}
-								var result = template(data);
+						getData(file.path, function (data) {
+							fse.readFile(file.path, settings.encode, function(err, source) {
+								if (source) {
+									var helperName = getHelperName(file);
+									var registerHelper = setHelper(helperName, source);
+									var template = handlebars.compile(source);
+									var result = getHtml(template, data);
 
-								var object = {
-									helperName: helperName,
-									html: result 
-								};
+									var object = {
+										helperName: helperName,
+										html: result
+									};
 
-								renderFile(object);
-							}
+									renderFile(object);
+								}
+							});
 						});
 					} catch (e) {
-						console.log("\x1b[41m", e, "\x1b[0m");
+						log(e, 'error');
 					}
 				}
+			})
+			.on('end', function () {
+				log('Files rendered', 'success');
 			});
+	}
+
+	var getData = function (fileName, callback) {
+		var jsonFile = fileName.replace(settings.fileExtension, '.json');
+
+		fse.stat(jsonFile, function(err, stat) {
+			var jsonData;
+			if (!err) {
+				delete require.cache[require.resolve(jsonFile)];
+				jsonData = require(jsonFile);
+			}
+
+			callback(jsonData);
+		});
 	}
 
 	var renderFile = function (fileInfo, file) {
@@ -57,14 +80,14 @@ var democritusRenderer = function (settings) {
 		fileName = fileArr[fileArr.length - 1];
 		fileArr.pop();
 		path = fileArr.join('/');
-		
-		if (!fse.statSync(path).isDirectory()) {
-			fse.mkdirSync(path);
-		}
-		
-		fse.open(filePath, 'w', function(err, fd) {
-			fse.writeFile(filePath, fileInfo.html, settings.encode, function (err) {
-				console.log('test', err);
+
+		mkdirp(path, function (err) {
+			fse.open(filePath, 'w', function(err, fd) {
+				fse.writeFile(filePath, fileInfo.html, settings.encode, function (err) {
+					if (err) {
+						log(err, 'error');
+					}
+				});
 			});
 		});
 	}
@@ -78,10 +101,48 @@ var democritusRenderer = function (settings) {
 		handlebars.registerPartial(name, source);
 	}
 
-	var settings = getSettings(settings);
-	var rootPath = getRootPath();
+	var getHtml = function (template, data) {
+		try {
+			return template(data);
+		}
+		catch(err) {
+			log(err.message, 'error');
+			return 'ERROR: ' + err.message;
+		}
+	}
 
-	getPatterns();
+	var cleanPaths = function (callback) {
+		rimraf(settings.publicPatternsPath, function () {
+			log('cleaning up patterns folder before recreate');
+			mkdirp(settings.publicPatternsPath, function(err) {
+				if (err) {
+					console.log(err, 'error');
+				}
+
+				callback();
+			});
+		});
+	}
+
+	var rootPath = getRootPath();
+	var settings = getSettings(settings);
+
+	init();
 }
 
 democritusRenderer();
+
+var log = function (message, type) {
+	var types = {
+			error: '\x1b[41m',
+			success: '\x1b[42m'
+		},
+		reset = '\x1b[0m',
+		color = reset;
+
+	if (type) {
+		color = types[type];
+	}
+
+	console.log(color, message, reset);
+}
